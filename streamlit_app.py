b543bf9fd1d63b6f0f3c9682e4a0dea92b3c7a26
poypoy98 <143189067+poypoy98@users.noreply.github.com>
2023-08-26 10:54:47 +1000

    Update streamlit_app.py
    Breakfast update

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,5 @@ st.title ("My Parents New Healthy Diner")
 
 st.header ("Breakfast Menu") 
 st.text ("Omega 3 & Blueberry Oatmeal")
-
+st.text ("Kale, Spinach & Rocket Smoothie")
+st.text ("Hard-boiled Free-range Egg")
