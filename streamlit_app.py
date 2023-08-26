883a5cc160b02c665b93d31685c04c577ff3e18c
poypoy98 <143189067+poypoy98@users.noreply.github.com>
2023-08-26 10:57:25 +1000

    Update streamlit_app.py
    added icons and avo toast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
 st.title ("My Parents New Healthy Diner")
-
+   
 st.header ("Breakfast Menu") 
-st.text ("Omega 3 & Blueberry Oatmeal")
-st.text ("Kale, Spinach & Rocket Smoothie")
-st.text ("Hard-boiled Free-range Egg")
+st.text ("🥣 Omega 3 & Blueberry Oatmeal")
+st.text ("🥗 Kale, Spinach & Rocket Smoothie")
+st.text ("🐔 Hard-boiled Free-range Egg")
+st.text ("🥑🍞 Avocado Toast")
