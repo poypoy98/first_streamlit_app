17a4ac14d96099dfe969ab80a174fd3d298c3017
poypoy98 <143189067+poypoy98@users.noreply.github.com>
2023-08-26 11:02:59 +1000

    Update streamlit_app.py
    typo pd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 st.title ("My Parents New Healthy Diner")
    
