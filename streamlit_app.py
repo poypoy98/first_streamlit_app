5ef35fdeafdca345432dd47dad50625fdf646229
poypoy98 <143189067+poypoy98@users.noreply.github.com>
2023-08-26 10:58:27 +1000

    Update streamlit_app.py
    added BYO smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ st.text ("🥣 Omega 3 & Blueberry Oatmeal")
 st.text ("🥗 Kale, Spinach & Rocket Smoothie")
 st.text ("🐔 Hard-boiled Free-range Egg")
 st.text ("🥑🍞 Avocado Toast")
+
+st.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
