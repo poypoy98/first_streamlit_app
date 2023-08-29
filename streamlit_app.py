8548161c479e364f75f99285e34128e1d6b2742d
poypoy98 <143189067+poypoy98@users.noreply.github.com>
2023-08-29 23:48:00 +1000

    Update streamlit_app.py
    added variable to fruit selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,10 @@ st.dataframe(my_fruit_list)
 st.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # write your own comment -what does the next line do?  changes text format
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
